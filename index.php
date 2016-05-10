34f8b6b8cf8e4120245b110ec0dcdc95a4f94d22
dalepotter <dalepotter@gmail.com>
2016-05-10 18:41:01 +0100

    Split homepage and add basic pricing page

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once 'vendor/autoload.php';
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader);
 
-if ( $_REQUEST['path'] == 'index.html' ){
+if ( $_REQUEST['path'] == 'index.html' || !array_key_exists('path', $_REQUEST) ){
 	// Render hompage
 	$twig->render('index.html');
 
