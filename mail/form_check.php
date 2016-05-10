fc3e34d68b2662e9bae381ef7fa6ae554fa397c0
dalepotter <dalepotter@gmail.com>
2016-05-10 23:29:30 +0100

    Add extra field to valid form fields

--- mail/form_check.php
+++ mail/form_check.php
@@ -3,7 +3,7 @@
 function is_form_data_valid($arr){
 	// Check that only allowed fields have been passed in
 	foreach($arr as $k => $v){
-		if ( !in_array($k, array('name', 'email', 'phone', 'property_postcode', 'num_properties', 'service')) ){
+		if ( !in_array($k, array('name', 'email', 'phone', 'property_postcode', 'num_properties', 'how_heard', 'service')) ){
 			return false;
 		}
 	}
