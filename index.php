27dea81c8842062acd89df26d5e74630ffefb5cd
dalepotter <dalepotter@gmail.com>
2016-05-10 18:22:07 +0100

    Add templating with Twig

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+// Serves as a redirect controller
+
+// Import Twig
+require_once 'vendor/autoload.php';
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader);
+
+if ( $_REQUEST['path'] == 'index.html' ){
+	// Render hompage
+	$twig->render('index.html');
+
+} elseif ( $_REQUEST['path'] == 'pricing.html' ){
+	// Render pricing page
+	$twig->render('pricing.html');
+}
+
+?>
